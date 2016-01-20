11fc06b0bcfdab596642809b7894a45a3db62daf
Thiago Andrade <thiagoh@gmail.com>
2016-01-20 12:30:42 -0300

    lambda and closure

---
+++ modules/lambda/src/main.cpp
@@ -0,0 +1,46 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Jan 20, 2016
+ *      Author: thiago
+ */
+
+#include <stdio.h>
+#include <string>
+#include <functional>
+
+using std::string;
+
+void call(std::function<void()> func) {
+
+   func();
+   func();
+}
+
+int main(int argc, char **argv) {
+
+   printf("\n\nBEGIN OF PROGRAM\n\n");
+
+   auto firstLambda = [] {printf("my first lambda\n");};
+   firstLambda();
+
+   string name = string("antonius carlo");
+   auto lambdaRef1 = [&] {printf("my lambda with outside ref %s\n", name.c_str());};
+   lambdaRef1();
+
+   // modifier 1
+   auto modifierLambda1 = [&] {name = name + " is the one!";};
+   modifierLambda1();
+   printf("New name is %s\n", name.c_str());
+
+   // modifier 2
+   [&] {name = name + " He is even better!";}();
+   printf("New name is %s\n", name.c_str());
+
+   auto modifierLambda3 = [&] {name = name + " He is almost the best!";};
+   call(modifierLambda3);
+   printf("New name is %s\n", name.c_str());
+
+   printf("\n\nEND OF PROGRAM\n");
+}
+
