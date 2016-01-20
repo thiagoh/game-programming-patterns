dfbdda5313cff86d779446bdbc54279313bfdb34
Thiago Andrade <thiagoh@gmail.com>
2016-01-20 11:42:17 -0300

    function pointer

--- modules/pointer-to-function/src/main.cpp
+++ modules/pointer-to-function/src/main.cpp
@@ -13,29 +13,57 @@
 using std::string;
 using namespace com::thiagoh;
 
+double div(double a, double b) {
+   return a / b;
+}
+
+double percent(double a, double b) {
+   return b * 100.0 / a;
+}
+
 int main(int argc, char **argv) {
-	
 
-	/*
-	* More about Poiter to member function at 
-	* http://www.codeguru.com/cpp/cpp/article.php/c17401/C-Tutorial-PointertoMember-Function.htm
-	*/
-	printf("\n\nBEGIN OF PROGRAM\n\n");
+   /*
+    * More about Poiter to member function at
+    * http://www.codeguru.com/cpp/cpp/article.php/c17401/C-Tutorial-PointertoMember-Function.htm
+    */
+   printf("\n\nBEGIN OF PROGRAM\n\n");
 
-	Foo animal("fox");
+   Foo animal("fox");
 
-	printf("animal.getValue: %s\n", animal.getValue("foo", "bar").c_str());
+   printf("animal.getValue: %s\n", animal.getValue("foo", "bar").c_str());
 
-	string (Foo::*_getValuePointer)(string, string) = &Foo::getValue;
+   string (Foo::*_getValuePointer)(string, string) = &Foo::getValue;
 
-	printf("(animal.*_getValuePointer): %s\n", (animal.*_getValuePointer)("foo", "bar").c_str());
+   printf("(animal.*_getValuePointer): %s\n", (animal.*_getValuePointer)("foo", "bar").c_str());
 
-	string (*processValuePointer)(string, string) = &Foo::processValue;
+   string (*processValuePointer)(string, string) = &Foo::processValue;
 
-	printf("processValuePointer: %s\n", processValuePointer("foo", "bar").c_str());
+   printf("processValuePointer: %s\n", processValuePointer("foo", "bar").c_str());
 
-	printf("(*processValuePointer): %s\n", (*processValuePointer)("foo", "bar").c_str());
+   printf("(*processValuePointer): %s\n", (*processValuePointer)("foo", "bar").c_str());
 
-	printf("\n\nEND OF PROGRAM\n");
+   /* the ampersand is actually optional */
+   double (*func1)(double, double) = &div;
+   /* the ampersand is actually optional */
+   double (*func2)(double, double) = &percent;
+   /* the ampersand is actually optional */
+   auto *func3 = &percent;
+   /* the ampersand is actually optional */
+   auto func4 = &percent;
+
+   printf("(*func1): %.2f\n", (*func1)(10, 5));
+   printf("func1: %.2f\n", func1(10, 5));
+
+   printf("(*func2): %.2f\n", (*func2)(10, 5));
+   printf("func2: %.2f\n", func2(10, 5));
+
+   printf("auto *func3: %.2f\n", func3(100, 5));
+   printf("auto func4: %.2f\n", func4(100, 9));
+
+   auto name = string("julius");
+   printf("auto string name: %s\n", name.c_str());
+
+   printf("\n\nEND OF PROGRAM\n");
 }
 
