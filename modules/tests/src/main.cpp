dcbbf288b792383b64df30836929d654a25868c3
Thiago Andrade <thiagoh@gmail.com>
2016-01-28 09:54:04 -0300

    tests

--- modules/tests/src/main.cpp
+++ modules/tests/src/main.cpp
@@ -9,58 +9,63 @@
 #include <Unit.h>
 #include <string>
 
+#include <cppunit/extensions/HelperMacros.h>
+
 using com::thiagoh::Unit;
 
 void test1() {
 
-	Unit* unit1 = new Unit("UNIT_NAME 1");
-	Unit* unit2 = unit1;
+   Unit* unit1 = new Unit("UNIT_NAME 1");
+   Unit* unit2 = unit1;
 
-	printf("unit1 pointer: %s\n", unit1->id().c_str());
+   printf("unit1 pointer: %s\n", unit1->id().c_str());
 
-	if (unit2 != NULL) {
-		printf("unit2 pointer: %s\n", unit2->id().c_str());
-	} else {
-		printf("unit2 pointer is null\n");
-	}
+   if (unit2 != NULL) {
+      printf("unit2 pointer: %s\n", unit2->id().c_str());
+   } else {
+      printf("unit2 pointer is null\n");
+   }
 
-	/*
-	 * http://stackoverflow.com/a/2910694/889213
-	 *
-	 * calling delete on a pointer does not set the pointer to NULL
-	 * it still points to the address of memory that previously was allocated
-	 */
+   /*
+    * http://stackoverflow.com/a/2910694/889213
+    *
+    * calling delete on a pointer does not set the pointer to NULL
+    * it still points to the address of memory that previously was allocated
+    */
 
-	delete unit1;
+   delete unit1;
 
-	/*
-	 * A pointer to memory that is no longer allocated is called a
-	 * pointer and accessing it will usually cause strange program behaviour
-	 * and crashes, since its contents are probably not what you expect
-	 */
-	unit1; // dangling pointer
-	unit2; // dangling pointer
+   /*
+    * A pointer to memory that is no longer allocated is called a
+    * pointer and accessing it will usually cause strange program behaviour
+    * and crashes, since its contents are probably not what you expect
+    */
+   unit1; // dangling pointer
+   unit2; // dangling pointer
 
-	unit1 = NULL; // not a dangling pointer anymore
-	unit2 = NULL; // not a dangling pointer anymore
+   CPPUNIT_ASSERT_MESSAGE("Should not point to 0", unit1 != 0);
+   CPPUNIT_ASSERT_MESSAGE("Should not point to 0", unit2 != 0);
 
-	printf("unit1 address: %p\n", unit1);
-	printf("unit2 address: %p\n", unit2);
+   unit1 = NULL; // not a dangling pointer anymore
+   unit2 = NULL; // not a dangling pointer anymore
 
-	if (unit2 != NULL) {
-		printf("unit2 pointer: %s\n", unit2->id().c_str());
-	} else {
-		printf("unit2 pointer is null\n");
-	}
+   printf("unit1 address: %p\n", unit1);
+   printf("unit2 address: %p\n", unit2);
+
+   if (unit2 != NULL) {
+      printf("unit2 pointer: %s\n", unit2->id().c_str());
+   } else {
+      printf("unit2 pointer is null\n");
+   }
 
 }
 
 int main(int argc, char **argv) {
 
-	printf("\n\nBEGIN OF PROGRAM\n\n");
+   printf("\n\nBEGIN OF PROGRAM\n\n");
 
-	test1();
+   test1();
 
-	printf("\n\nEND OF PROGRAM\n");
+   printf("\n\nEND OF PROGRAM\n");
 }
 
