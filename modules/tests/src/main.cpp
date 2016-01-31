0d68c218970e8b8863609101fcefd1f4f6c6c991
Thiago Andrade <thiagoh@gmail.com>
2016-01-31 19:00:40 -0300

    implicit cast with cppunit tests

--- modules/tests/src/main.cpp
+++ modules/tests/src/main.cpp
@@ -104,7 +104,7 @@ public:
       CPPUNIT_ASSERT_MESSAGE("Should not point to 0", true);
    }
 
-   void test_delete_copied_pointer() {
+   void test_delete_local_pointer() {
 
       printf("test_delete_copied_pointer started\n");
 
@@ -171,9 +171,59 @@ public:
       printf("test_delete_reference_pointer ended\n\n");
    }
 
+   void test_delete_copy_pointer() {
+
+      printf("test_delete_copy_pointer started\n\n");
+
+      Unit* unit1 = new Unit("UNIT_NAME test_delete_reference_pointer");
+      Unit* unit2 = unit1;
+
+      _test_delete_copy_pointer(unit1, unit2);
+
+      CPPUNIT_ASSERT_MESSAGE("Should not point to 0", unit1 == 0);
+      CPPUNIT_ASSERT_MESSAGE("Should not point to 0", unit2 == 0);
+
+      CPPUNIT_ASSERT_MESSAGE("Should not point to NULL", unit1 == NULL);
+      CPPUNIT_ASSERT_MESSAGE("Should not point to NULL", unit2 == NULL);
+
+      printf("test_delete_reference_pointer ended\n\n");
+   }
+
 private:
 
    void _test_delete_reference_pointer(Unit* &unit1, Unit* &unit2) {
+
+      /*
+       * http://stackoverflow.com/a/2910694/889213
+       *
+       * calling delete on a pointer does not set the pointer to NULL
+       * it still points to the address of memory that previously was allocated
+       */
+
+      delete unit1;
+
+      /*
+       * A pointer to memory that is no longer allocated is called a
+       * pointer and accessing it will usually cause strange program behaviour
+       * and crashes, since its contents are probably not what you expect
+       */
+      unit1; // dangling pointer
+      unit2; // dangling pointer
+
+      CPPUNIT_ASSERT_MESSAGE("Should not point to 0", unit1 != 0);
+      CPPUNIT_ASSERT_MESSAGE("Should not point to 0", unit2 != 0);
+
+      unit1 = NULL; // not a dangling pointer anymore
+      unit2 = NULL; // not a dangling pointer anymore
+
+      CPPUNIT_ASSERT_MESSAGE("Should not point to 0", unit1 == 0);
+      CPPUNIT_ASSERT_MESSAGE("Should not point to 0", unit2 == 0);
+
+      CPPUNIT_ASSERT_MESSAGE("Should not point to NULL", unit1 == NULL);
+      CPPUNIT_ASSERT_MESSAGE("Should not point to NULL", unit2 == NULL);
+   }
+
+   void _test_delete_copy_pointer(Unit* unit1, Unit* unit2) {
 
       /*
        * http://stackoverflow.com/a/2910694/889213
@@ -218,7 +268,9 @@ int main(int argc, char **argv) {
    // ####
    CppUnit::TestSuite suite("Simple Tests");
    suite.addTest(
-         new CppUnit::TestCaller<GeneralTest>("test_delete_copied_pointer", &GeneralTest::test_delete_copied_pointer));
+         new CppUnit::TestCaller<GeneralTest>("test_delete_local_pointer", &GeneralTest::test_delete_local_pointer));
+   suite.addTest(
+         new CppUnit::TestCaller<GeneralTest>("test_delete_copy_pointer", &GeneralTest::test_delete_copy_pointer));
    suite.addTest(
          new CppUnit::TestCaller<GeneralTest>("test_delete_reference_pointer",
                &GeneralTest::test_delete_reference_pointer));
